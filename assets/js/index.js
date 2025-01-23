f3c197d36a4e6bd847b215a64438a80a814824f0
xAldox995 <valzanialdo@gmail.com>
2025-01-23 13:28:20 +0100

    true end

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,17 +4,19 @@ const ul = document.getElementById("tasksList")
 const addTask = function (ev) {
     ev.preventDefault()
     const newTask = document.createElement('li')
-    const delBtn = document.createElement('button')
-    delBtn.style.backgroundColor ='black'
-    delBtn.innerText='❌'
-    newTask.innerHTML = input.value
-    newTask.addEventListener('click',(ev) =>{
-        newTask.classList.toggle('complete')
-    })
-    delBtn.addEventListener('click',(ev) =>{
-        ul.removeChild(newTask)
-    })
-    newTask.appendChild(delBtn)
-    ul.appendChild(newTask)
-    input.value = ''
+    if (input.value !== '') {
+        newTask.innerHTML = input.value
+        newTask.addEventListener('click', (ev) => {
+            newTask.classList.toggle('complete')
+        });
+        const delBtn = document.createElement('button')
+        delBtn.style.backgroundColor = 'black'
+        delBtn.innerText = '❌'
+        delBtn.addEventListener('click', (ev) => {
+            ul.removeChild(newTask)
+        });
+        newTask.appendChild(delBtn)
+        ul.appendChild(newTask)
+        input.value = ''
+    }
 }
